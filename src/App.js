4da75db2bad76a3d2cc19866f6f0bebe7bf0d637
Kimyeongsan <apua34@gmail.com>
2021-09-05 19:01:44 +0900

    Test Splash Add

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import SplashScreen from 'react-native-splash-screen';
 import MainScreen from './screens/MainScreen';
 
 function App() {
-  
+
+  // Splash Add : 1초 후 Test 화면 이동
   useEffect(() => {
     setTimeout(() => {
       SplashScreen.hide();
