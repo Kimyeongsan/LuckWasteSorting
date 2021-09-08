688d051a3c4929ab2604e955f28cb935eba072d3
Kimyeongsan <apua34@gmail.com>
2021-09-08 23:41:02 +0900

    LoginBefore : 화면 Design 추가
    폰트 적용 및 버튼 추가 완료
    추후 : 로그인 완료시, 재실행 할 경우 화면이 출력되지 않는 기능 추가 예정

--- src/screens/LoginBeforeScreen.js
+++ src/screens/LoginBeforeScreen.js
@@ -1,22 +1,75 @@
 import React from 'react';
 import styled from 'styled-components/native';
 
-import { Button } from 'react-native';
+import { ImageBackground } from 'react-native';
 
 
 const Container = styled.View`
   width: 100%;
   height: 100%;
-  background-color: #CFDEF3;
+  backgroundColor: rgba(0,0,0,0.4);
+`;
+
+const Background = styled(ImageBackground)`
+  width: 100%;
+  height: 100%;
+  resizeMode: cover;
+  position: absolute;
+`;
+
+const TitleContainer = styled.View`
+  width: 100%;
+  height: 150px;
+  marginTop: 132px;
+  marginLeft: 23px;
 `;
 
 const Title = styled.Text`
-  flex: 1;
-	align-self: center;
-  marginTop: 50px;
-	fontSize: 20px;
-  font-weight: bold;
+	align-self: flex-start;
+  color: white;
+	fontSize: 50px;
+  font-family: Kameron-Bold;
 `;
+
+const ContentContainer = styled.View`
+  width: 100%;
+  height: 150px;
+  marginTop: 35px;
+  marginLeft: 23px;
+`;
+
+const Content = styled.Text`
+  marginBottom: 13px;
+	align-self: flex-start;
+  color: white;
+	fontSize: 18px;
+  font-family: Kameron-Bold;
+`;
+
+const NomalContent = styled.Text`
+	align-self: flex-start;
+  color: white;
+	fontSize: 18px;
+`;
+
+const StartButton = styled.TouchableOpacity`
+  width: 253px;
+  height: 60px;
+	align-self: center;
+  justifyContent: center;
+  marginTop: 115px;
+  borderWidth: 2px;
+  borderColor: white;
+`;
+
+const StartText = styled.Text`
+	align-self: center;
+  color: white;
+	fontSize: 30px;
+  font-family: Kadwa-Bold;
+  borderColor: white;
+`;
+
 
 // Login 되기 전 화면 
 // 혹은 Login 이 안됬을 경우 출력 될 화면
@@ -25,16 +78,33 @@ const Title = styled.Text`
 // Login으로 넘어갈 Button 제작 필요
 
 
-const LoginBeforeScreen = ({navigation}) => {
+const LoginBeforeScreen = ({ navigation }) => {
   return (
-    <Container>
-      <Title>LoginBefore Screen</Title>
+    <Background
+      source={require("../../assets/img/login_background.png")} >
 
-      <Button
-        title='Login Screen'
-        onPress={() => navigation.navigate('login')} />
+      <Container>
 
-    </Container>
+        <TitleContainer>
+          <Title>gather</Title>
+          <Title>tomorrow</Title>
+        </TitleContainer>
+
+        <ContentContainer>
+
+          <Content>기분좋은 환경실천, 내일을 모으다</Content>
+          <NomalContent>재활용 분리를 통해</NomalContent>
+          <NomalContent>깨끗한 하루하루를 모아보세요</NomalContent>
+
+        </ContentContainer>
+
+        <StartButton
+          onPress={() => navigation.navigate('login')}>
+          <StartText>Start Login</StartText>
+        </StartButton>
+
+      </Container>
+    </Background>
   );
 
 }
