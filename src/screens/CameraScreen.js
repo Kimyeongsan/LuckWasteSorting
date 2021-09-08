013ade61bbf3a6a0dd51917ccf91ea50a5e480db
Kimyeongsan <apua34@gmail.com>
2021-09-08 17:50:16 +0900

    Camera : Tensorflow Lite Add(Test)

--- src/screens/CameraScreen.js
+++ src/screens/CameraScreen.js
@@ -1,30 +1,75 @@
-import React from 'react';
-import styled from 'styled-components/native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { RNCamera } from 'react-native-camera-tflite';
+import outputs from '../../Output.json';
+import _ from 'lodash';
 
-const Container = styled.View`
-  width: 100%;
-  height: 100%;
-  background-color: #CFDEF3;
-`;
+let _currentInstant = 0;
 
-const Title = styled.Text`
-  flex: 1;
-	align-self: center;
-  marginTop: 50px;
-	fontSize: 20px;
-  font-weight: bold;
-`;
+export default class CameraScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      time: 0,
+      output: ""
+    };
+  }
 
-// Camera 검색 화면
-// Tensorflow Lite 적용 예정
-const CameraScreen = () => {
+  processOutput({ data }) {
+    const probs = _.map(data, item => _.round(item / 255.0, 0.02));
+    const orderedData = _.chain(data).zip(outputs).orderBy(0, 'desc').map(item => [_.round(item[0] / 255.0, 2), item[1]]).value();
+    const outputData = _.chain(orderedData).take(3).map(item => `${item[1]}: ${item[0]}`).join('\n').value();
+    const time = Date.now() - (_currentInstant || Date.now());
+    const output = `Guesses:\n${outputData}\nTime:${time} ms`;
+    this.setState(state => ({
+      output
+    }));
+    _currentInstant = Date.now();
+  }
 
-  return (
-    <Container>
-      <Title>Camera Screen</Title>
-    </Container>
-  );
-
+  render() {
+    const modelParams = {
+      file: "mobilenet_v1_1.0_224_quant.tflite",
+      inputDimX: 224,
+      inputDimY: 224,
+      outputDim: 1001,
+      freqms: 0
+    };
+    return (
+      <View style={styles.container}>
+        <RNCamera
+          ref={ref => {
+            this.camera = ref;
+          }}
+          style={styles.preview}
+          type={RNCamera.Constants.Type.back}
+          flashMode={RNCamera.Constants.FlashMode.on}
+          permissionDialogTitle={'Permission to use camera'}
+          permissionDialogMessage={'We need your permission to use your camera phone'}
+          onModelProcessed={data => this.processOutput(data)}
+          modelParams={modelParams}
+        >
+          <Text style={styles.cameraText}>{this.state.output}</Text>
+        </RNCamera>
+      </View>
+    );
+  }
 }
 
-export default CameraScreen;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: 'black'
+  },
+  preview: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  cameraText: {
+    color: 'white',
+    fontSize: 18,
+    fontWeight: 'bold'
+  }
+});
